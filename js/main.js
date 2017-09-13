b46cec1391bf5c00174b21389803011e34ed0232
Sean R Reid <sean@torchcodelab.com>
2017-09-13 15:22:25 -0400

    Added solution

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 var dropdownTrigger = document.getElementById('dropdownTrigger'),
     dropdownMenu = document.getElementById('dropdownMenu');
+
+var menuToggle = function() {
+    if(dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+
+    } else {
+        dropdownMenu.style.display = "block";
+    }
+};
+
+dropdownTrigger.addEventListener('click', menuToggle);
