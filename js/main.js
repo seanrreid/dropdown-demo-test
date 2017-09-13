053e50e67a912893f845a75b19b0041b1eb22e33
Sean R Reid <sean@torchcodelab.com>
2017-09-13 19:44:43 -0400

    udpated the solution to use class swapping

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 var dropdownTrigger = document.getElementById('dropdownTrigger'),
     dropdownMenu = document.getElementById('dropdownMenu');
 
-var menuToggle = function() {
-    if(dropdownMenu.style.display === "block") {
-        dropdownMenu.style.display = "none";
-
+var classSwap = function(e,className1,className2) {
+    if (e.classList.contains(className1)) {
+        e.classList.remove(className1);
     } else {
-        dropdownMenu.style.display = "block";
+        e.classList.add(className1);
     }
+    if (e.classList.contains(className2)) {
+        e.classList.remove(className2);
+    } else {
+        e.classList.add(className2);
+    }
+};
+
+var menuToggle = function() {
+    classSwap(dropdownMenu, "visibleMenu", "hiddenMenu");
 };
 
 dropdownTrigger.addEventListener('click', menuToggle);
